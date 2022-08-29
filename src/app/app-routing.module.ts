18827839d794c2945ffe45bc876c11e00df11283
Jean da Rocha Vertuoso <jean_b123@hotmail.com>
2022-08-29 18:52:14 -0300

    add alteracoes home component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,7 @@ const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: AutenticacaoComponent },
   { path: 'contato', component: ContatoComponent },
-  { path: '**', component: Error404Component },
+  { path: '**', component: Error404Component }
   ];
 
   @NgModule({
