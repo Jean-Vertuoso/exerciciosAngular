70806b572d366bb1edc6cd0a548f8d6c48a86845
Jean da Rocha Vertuoso <jean_b123@hotmail.com>
2022-08-29 15:39:12 -0300

    add detalhes lista6

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from "@angular/router";
+
 import { AutenticacaoComponent } from './autenticacao/autenticacao.component';
 import { ContatoComponent } from './pages/contato/contato.component';
 import { Error404Component } from './pages/error404/error404.component';
 import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent },
+  { path: '', component: HomeComponent },
   { path: 'login', component: AutenticacaoComponent },
   { path: 'contato', component: ContatoComponent },
   { path: '**', component: Error404Component },
